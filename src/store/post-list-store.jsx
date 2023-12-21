84901a58cba16b8507aa0064954c23d5f9742897
Aditya <aditya.jhab76@gmail.com>
2023-12-21 23:29:31 +0530

    first commit

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,76 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload, ...currPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = (userId, postTitle, postBody, reactions, tags) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: {
+        id: Date.now(),
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      },
+    });
+  };
+
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Mumbai",
+    body: "Hey Connections, heading to Mumbai for my new job position.",
+    reactions: 8,
+    userID: "user-1",
+    tags: ["Mumbai", "Work", "Opportunities"],
+  },
+  {
+    id: "8",
+    title: "Completed B.tech",
+    body: "Hey Connections, finally done with my 4 year long degree of computer Science, had lots of ups and downs but eventually things went pretty well.",
+    reactions: 18,
+    userID: "user-2",
+    tags: ["Graduation", "Unbelievable", "Opportunities"],
+  },
+];
+
+export default PostListProvider;
